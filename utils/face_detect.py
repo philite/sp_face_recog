7fbdd59d5d7f2b5664700364e5e8084ce958306d
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2018-10-14 22:19:37 +0700

    add new line

--- utils/face_detect.py
+++ utils/face_detect.py
@@ -6,7 +6,7 @@ import cv2
 
 detector = dlib.get_frontal_face_detector()
 win = dlib.image_window()
-path = ""
+path = "test.jpg"
 if len(sys.argv) >= 1:
     for f in sys.argv[1:]:
         print('Processing image {}'.format(f))
@@ -15,6 +15,14 @@ if len(sys.argv) >= 1:
         print('Number of faces detected: {}'.format(len(dets)))
         for i, d in enumerate(dets):
             print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
+else:
+    print('Processing image {}'.format(path))
+    img = cv2.readim(path)
+    dets = detector(img, 1)
+    print('Number of faces detected: {}'.format(len(dets)))
+    for i, d in enumerate(dets):
+        print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
+
 
     win.clear_overlay()
     win.set_image(img)
