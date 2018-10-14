1cf603ece21d6c1aec833b3584847d45b108c292
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2018-10-14 23:05:47 +0700

    z

--- utils/face_detect.py
+++ utils/face_detect.py
@@ -1,23 +1,26 @@
 
 import sys
-import dlib
-import scipy
 import cv2
+import dlib
 
 detector = dlib.get_frontal_face_detector()
 win = dlib.image_window()
-path = "test.jpg"
+path = 'test.jpg'
 if len(sys.argv) >= 1:
     for f in sys.argv[1:]:
         print('Processing image {}'.format(f))
-        img = cv2.readim(f)
+        img = dlib.load_rgb_image(f)
+        #img = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
+        #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         dets = detector(img, 1)
         print('Number of faces detected: {}'.format(len(dets)))
         for i, d in enumerate(dets):
             print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
 else:
     print('Processing image {}'.format(path))
-    img = cv2.readim(path)
+    img = dlib.load_rgb_image(path)
+    #img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     dets = detector(img, 1)
     print('Number of faces detected: {}'.format(len(dets)))
     for i, d in enumerate(dets):
