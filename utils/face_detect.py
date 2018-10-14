95ee596c9250604a8b1644246e063d3354aa10dd
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2018-10-14 23:31:43 +0700

    recoded

--- utils/face_detect.py
+++ utils/face_detect.py
@@ -6,7 +6,7 @@ import dlib
 detector = dlib.get_frontal_face_detector()
 win = dlib.image_window()
 path = 'test.jpg'
-if len(sys.argv) >= 1:
+if len(sys.argv) > 0:
     for f in sys.argv[1:]:
         print('Processing image {}'.format(f))
         img = dlib.load_rgb_image(f)
@@ -16,7 +16,13 @@ if len(sys.argv) >= 1:
         print('Number of faces detected: {}'.format(len(dets)))
         for i, d in enumerate(dets):
             print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
-else:
+        win.clear_overlay()
+        win.set_image(img)
+        win.set_title('Number of faces detected: {}'.format(len(dets)))
+        win.add_overlay(dets, color=dlib.rgb_pixel(255,255,0))
+        dlib.hit_enter_to_continue()
+
+elif len(sys.argv) is 0:
     print('Processing image {}'.format(path))
     img = dlib.load_rgb_image(path)
     #img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
@@ -25,9 +31,11 @@ else:
     print('Number of faces detected: {}'.format(len(dets)))
     for i, d in enumerate(dets):
         print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
-
-
     win.clear_overlay()
-    win.set_image(img)
-    win.add_overlay(dets)
+    win.set_title('Number of faces detected: {}'.format(len(dets)))
+    win.add_overlay(dets, color=dlib.rgb_pixel(255,255,0))
     dlib.hit_enter_to_continue()
+
+
+
+
