d34530f5b3e27065c560bc6bbbeea927b839a73e
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2018-10-15 11:56:55 +0700

    recode

--- utils/face_detect.py
+++ utils/face_detect.py
@@ -5,8 +5,9 @@ import dlib
 
 detector = dlib.get_frontal_face_detector()
 win = dlib.image_window()
-path = 'test.jpg'
-if len(sys.argv) > 0:
+path = [] #['data/test.jpg', 'data/1.jpg', 'data/2.jpg', 'data/3.jpg', 'data/4.jpg', 'data/5.jpg', 'data/6.jpg', 'data/7.jpg', 'data/8.jpg']
+
+if len(sys.argv) > 1:
     for f in sys.argv[1:]:
         print('Processing image {}'.format(f))
         img = dlib.load_rgb_image(f)
@@ -22,20 +23,18 @@ if len(sys.argv) > 0:
         win.add_overlay(dets, color=dlib.rgb_pixel(255,255,0))
         dlib.hit_enter_to_continue()
 
-elif len(sys.argv) is 0:
-    print('Processing image {}'.format(path))
-    img = dlib.load_rgb_image(path)
-    #img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    dets = detector(img, 1)
-    print('Number of faces detected: {}'.format(len(dets)))
-    for i, d in enumerate(dets):
-        print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
-    win.clear_overlay()
-    win.set_title('Number of faces detected: {}'.format(len(dets)))
-    win.add_overlay(dets, color=dlib.rgb_pixel(255,255,0))
-    dlib.hit_enter_to_continue()
-
-
-
-
+else:
+    for f in path:
+        print('Processing image {}'.format(f))
+        img = dlib.load_rgb_image(f)
+        #img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        #img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        dets = detector(img, 1)
+        print('Number of faces detected: {}'.format(len(dets)))
+        for i, d in enumerate(dets):
+            print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
+        win.clear_overlay()
+        win.set_image(img)
+        win.set_title('Number of faces detected: {}'.format(len(dets)))
+        win.add_overlay(dets, color=dlib.rgb_pixel(255,255,0))
+        dlib.hit_enter_to_continue()
--- utils/vdodetect.py
+++ utils/vdodetect.py
@@ -3,18 +3,17 @@ import sys
 import cv2
 import dlib
 
-file = 'test.MKV'
+file = 'test.mp4'
 cap = cv2.VideoCapture(file)
 detector = dlib.get_frontal_face_detector()
 win = dlib.image_window()
 while True:
     ret, frame = cap.read()
-    img = dlib.load_rgb_image(frame)
-    dets = detector(img, 1)
+    dets = detector(frame, 1)
     print('Number of faces detected: {}'.format(len(dets)))
-    for i, d in enumerate(dets):
-        print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
+    #for i, d in enumerate(dets):
+     #   print('Detection {}: Left: {} Top: {} Right: {} Bottom: {}'.format(i, d.left(), d.top(), d.right(), d.bottom()))
     win.clear_overlay()
-    win.set_image(img)
+    win.set_image(frame)
     win.set_title('Number of faces detected: {}'.format(len(dets)))
     win.add_overlay(dets, color=dlib.rgb_pixel(255,255,0))
