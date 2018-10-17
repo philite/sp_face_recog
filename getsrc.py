243a253a0780256c29c8681d51f969a89fd0d4ee
Yuttapichai Lamnaonan <y.lamnaonan@gmail.com>
2018-10-17 11:44:00 +0700

    reconstruct

--- getsrc.py
+++ getsrc.py
@@ -7,4 +7,4 @@ from src import webcamstream , Face
 #webcam.start()
 #webcam.save()
 face = Face.Face()
-face.detect(['G:/My Drive/46_48_group/Keyakizaka46/Techi/2312313.jpg'])
+face.detect(['data/test.jpg'])
